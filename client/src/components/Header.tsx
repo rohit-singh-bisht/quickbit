50e26e3feaad5b608c24c867064d80e00017fa53
rohit-singh-bisht <thersbisht@gmail.com>
2023-11-05 21:46:42 +0530

    login and register button fix

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import { useThemeContext } from "../context/ColorThemeContext";
 import styled from "styled-components";
 import { Styles } from "../interfaces";
+import { Link } from "react-router-dom";
 
 const HeaderStyled = styled.header<Styles>`
   padding: 4rem 5.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   .logo {
     font-size: 3.6rem;
     font-style: normal;
@@ -16,6 +20,35 @@ const HeaderStyled = styled.header<Styles>`
     -webkit-background-clip: text;
     display: inline-block;
   }
+  .login__buttons {
+    display: flex;
+    gap: 2rem;
+    button {
+      padding: 2.1rem 3.7rem;
+      font-size: 1.6rem;
+      font-weight: 600;
+      line-height: 1.8rem;
+      color: #fff;
+      border-radius: 4.8rem;
+      transition: all 0.3s;
+      &.login {
+        border: 0.1rem solid ${({ themeColor }) => themeColor?.border};
+        background: ${({ themeColor }) => themeColor?.textGrey};
+        &:hover {
+          box-shadow: 0rem 0rem 2.2rem 0rem rgba(20, 78, 227, 0.38);
+          background: ${({ themeColor }) => themeColor?.primaryBlue};
+        }
+      }
+      &.register {
+        border: 0.1rem solid ${({ themeColor }) => themeColor?.primaryBlue};
+        background: ${({ themeColor }) => themeColor?.primaryBlue};
+        &:hover {
+          background: ${({ themeColor }) => themeColor?.textGrey};
+          box-shadow: 0rem 0rem 2.2rem 0rem rgba(20, 78, 227, 0.38);
+        }
+      }
+    }
+  }
 `;
 
 const Header: React.FC = () => {
@@ -23,7 +56,15 @@ const Header: React.FC = () => {
 
   return (
     <HeaderStyled themeColor={themeColor}>
-      <div className="logo">Quickbit</div>
+      <Link to={"/"} className="logo">
+        Quickbit
+      </Link>
+      <div className="login__buttons">
+        <button className="login">Login</button>
+        <button type="button" className="register">
+          Register Now
+        </button>
+      </div>
     </HeaderStyled>
   );
 };
