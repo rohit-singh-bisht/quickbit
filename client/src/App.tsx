f647fee09e3ab156032a7449be1888afe70dccfe
rohit-singh-bisht <thersbisht@gmail.com>
2023-11-13 22:32:13 +0530

    initial new page added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import Header from "./components/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ToggleTheme from "./components/ToggleTheme";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
         </Routes>
       </BrowserRouter>
     </>
